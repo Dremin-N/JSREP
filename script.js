045a239220fe042ab0b9ab3964f2628b0cdb6648
Dremin-N <dr.kolka19@gmail.com>
2024-03-01 00:23:40 +0500

    dadsas

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ function showTime(day = new Date(2025, 1, 7, 23)) {
 setInterval(function () {
   box.firstElementChild.innerText = `До окончания осталось ${showTime()}`;
 }, 1000);
+
+//! Задача 2.1
+let rootElement = document.querySelector("#root");
+let new_element = document.createElement("p");
+console.log(rootElement);
+new_element.innerText = "Ура, мой скрипт отработал верно";
+rootElement.appendChild(new_element);
+
+//! Задача 2.2 В HTML-документе есть тег <p> c идентификатором title. Напишите скрипт, который создает новый элемент <h2> в переменной new_element с текстовым содержимым Сегодня солнечно, минус 15 и добавляет новый элемент сразу после тега с идентификатором title.
+
+new_element = document.createElement("h2");
+let title = document.querySelector("#title");
+
+new_element.innerText = "Сегодня солнечно, минус 15";
+console.log(title);
+document.body.insertBefore(new_element, title.nextSibling);
