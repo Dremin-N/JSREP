695abeace7b686d4e4eaaf343d3a4be6b97efaa3
Dremin-N <dr.kolka19@gmail.com>
2024-03-01 15:30:25 +0500

    merge

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ while (n--) {
 
 const box = document.querySelector(".box");
 box.innerHTML = `<div class="card"></div>`;
-
+const card = document.querySelector(".card");
 for (let val of data) {
   box.innerHTML += `<div class="card" style="background-color: ${
     colors[getNumber(colors.length)]
@@ -48,3 +48,25 @@ function showTime(day = new Date(2025, 1, 7, 23)) {
 setInterval(function () {
   box.firstElementChild.innerText = `До окончания осталось ${showTime()}`;
 }, 1000);
+
+let title = document.createElement("h1");
+title.innerHTML = "Самая важная страница на свете";
+
+title.id = "my-title";
+title.className = "caption red";
+
+console.log(title);
+
+document.body.insertBefore(title, box);
+
+let paragraph = document.createElement("p");
+paragraph.innerText = "Text here";
+
+let link = document.createElement("a");
+link.innerText = "link";
+link.href = "";
+
+let paragraph2 = document.createElement("p");
+paragraph2.innerText = "More text";
+
+document.body.append(paragraph, link, paragraph2);
